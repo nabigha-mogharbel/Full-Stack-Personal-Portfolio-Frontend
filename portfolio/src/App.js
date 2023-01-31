0ed8b54909f403bc10add0ea5636109c831e9aff
Mazen El Ali <mazenelali>
2023-01-31 18:04:15 +0200

    Front end files edit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-// import About from "./components/About"
-// import Skills from "./components/Skills"
-// import Projects from "./components/Projects"
-// import Resume from "./components/Resume"
-// import SideBar from "./components/SideBar"
-// import Contact from "./components/Contact"
+// import About from "./About_component/About"
+// import Skills from "./Skills_component/Skills"
+// import Projects from "./Projects_component/Projects"
+// import Resume from "./Resume_component/Resume"
+// import SideBar from "./Sidebar_component/SideBar"
+// import Contact from "./Contact_component/Contact"
 
 
 function App() {

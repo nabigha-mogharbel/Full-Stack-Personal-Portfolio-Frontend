391e7f600da86a70bee4b277899f0fd60f33c26d
AWIKSARAH <awiksarah@gmail.com>
2023-02-04 18:09:55 +0200

    AxiosLogin

---
+++ portfolio/src/About_component/About.js
@@ -0,0 +1,31 @@
+// import React, { Component } from "react";
+// import axios from "axios";
+// // import '../Login_component/Login.css'
+
+// class About extends Component {
+//   state = {
+  
+//     error: "",
+//     success: [],
+//   };
+
+
+//   componentDidMount() {
+//     axios
+//       .get("http://localhost:5000/admin/")
+//       .then(response => {
+//         this.setState({ success: response.data });
+//         console.log(response.data);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
+
+//   render() {
+//     return (
+//     <><h1> helloooooo </h1></>
+//     );
+//   }
+// }
+// export default About;
--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,29 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login_component/Login';
-// import About from "./About_component/About"
-// import Skills from "./Skills_component/Skills"
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Login_component/Login";
+// import About from "./About_component/About";
+// import Skills from "./Skills_component/Skills";
 // import Projects from "./Projects_component/Projects"
 // import Resume from "./Resume_component/Resume"
 // import SideBar from "./Sidebar_component/SideBar"
 // import Contact from "./Contact_component/Contact"
 // import Login from "./Login_component/Login"
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
-  state = {  } 
-  render() { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  handleLogin = () => {
+    this.setState({ isLoggedIn: true });
+  };
+
+  render() {
     return (
       <>
-    
-      <Login></Login>
+        {!this.state.isLoggedIn ? (
+          <Login onLogin={this.handleLogin} />
+        ) : (
+          <>
+            {/* <About /> */}
 
- 
+          </>
+        )}
       </>
     );
   }
 }
- 
-export default App;
 
+
+
+
+
+export default App;

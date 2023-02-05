5aef8e00143aac66080cf1547c3cf91a42b8e110
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-02-05 16:17:28 +0200

    add sidebar

--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -3,7 +3,7 @@ import About from "../About_component/About"
 import Projects from "../Project_Component/Projects"
 import Resume from "../Resume_component/Resume"
 import Contact from "../Contact_component/Contact"
-
+import Sidebar from "../Sidebar_component/Sidebar"
 export default class Portfolio extends React.Component{
     constructor(props){
         super(props)
@@ -11,6 +11,7 @@ export default class Portfolio extends React.Component{
     render(){
         return(<>
             <About />
+            <Sidebar/>
             <Projects />
             <Resume />
             <Contact/>

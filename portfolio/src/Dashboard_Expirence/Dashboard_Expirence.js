08e229b3934f51c34b1c5ce6e99813b3365cfdac
AWIKSARAH <awiksarah@gmail.com>
2023-02-08 15:16:10 +0200

    Experience Axios Without Post

--- portfolio/src/Dashboard_Expirence/Dashboard_Expirence.js
+++ portfolio/src/Dashboard_Expirence/Dashboard_Expirence.js
@@ -1,39 +1,47 @@
-import React from "react"
+import React from "react";
 import Expireience from "./Expiences";
-import "../Dashboard_Expirence/Expirence.css"
-
+import "../Dashboard_Expirence/Expirence.css";
+import axios from "axios";
 class DashboardExpirience extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    _id: "63d28f75bf3f9bab4b38bb22",
+    name: "first project",
+    companyName: "Codi.Tech",
+    startDate: "2006-05-13T00:00:00.000Z",
+    description: "Create a portfolio",
+    data: [],
+  };
+  componentDidMount() {
+    this.getData();
+  }
+  getData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5008/dashboard/experience/`
+      );
+      this.setState({ data: response.data.response });
+      console.log(response.data.response);
+    } catch (error) {
+      console.error(error);
     }
-    state = { Experience : [ {
-        _id: "63d28f75bf3f9bab4b38bb22",
-        name: "first project",
-        companyName: "Codi.Tech",
-        startDate: "2006-05-13T00:00:00.000Z",
-        description: "Create a portfolio",
-        __v: 0
-      },
-      {
-        _id: "63d2c0bb9492ae31d0d77ba8",
-        name: "Third",
-        companyName: "DD",
-        startDate: "2000-05-13T00:00:00.000Z",
-        endDate: "2005-05-13T00:00:00.000Z",
-        description: "Create a portfolio",
-        __v: 0
-      },]
+  };
 
-      }
-    render() { 
-        return ( <div className="dashboard-section">
-        <h1>Experience</h1><hr />
-        <main className="container-column">{this.state.Experience.map((objec)=>{
-            return <Expireience Expir={objec} key={objec._id}/>
-        })}
+  render() {
+    return (
+      <div className="dashboard-section">
+        <h1>Experience</h1>
+        <hr />
+        <main className="container-column">
+          {this.state.data.map((objec) => {
+            return <Expireience Expir={objec} key={objec._id} />;
+          })}
         </main>
-    </div> );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default DashboardExpirience;
--- portfolio/src/Dashboard_Expirence/Expiences.js
+++ portfolio/src/Dashboard_Expirence/Expiences.js
@@ -1,63 +1,154 @@
 import React from "react";
-import edit from "../edit.svg"
-import trash from "../trash.svg"
-import send from "../send.svg"
-
+import edit from "../edit.svg";
+import trash from "../trash.svg";
+import send from "../send.svg";
+import axios from "axios";
 class Expirence extends React.Component {
-    state = {  
-        isvisible:true
+  state = {
+    isvisible: true,
+    id: this.props.Expir._id,
+    data: {
+      name: this.props.Expir._name,
+      startDate: this.props.StartDate,
+      endDate: this.props.EndDate,
+      companyName: this.props.CompanyName,
+      description: this.props.Description,
+    },
+  };
+  toggelvisible = () => {
+    this.setState({ isvisible: false });
+    if (this.state.isvisible == false) {
+      this.setState({ isvisible: true });
     }
-    toggelvisible = () => {
-        this.setState({ isvisible: false })
-        if (this.state.isvisible == false) {
-            this.setState({ isvisible: true })
-        }
+  };
+  updateData = async (data, id) => {
+    id = this.state.id;
+    const experienceData = {
+      name: this.state.data.name,
+      startDate: this.state.data.startDate,
+      endDate: this.state.data.endDate,
+      companyName: this.state.data.companyName,
+      description: this.state.data.description,
+    };
+    console.log(experienceData + "\n" + id);
+    try {
+      const response = await axios.put(
+        `http://localhost:5008/dashboard/experience/update/${id}`,
+         experienceData
+      );
+    
+      this.setState({ data: response.data.response });
+      console.log(response.data.response);
+    } catch (error) {
+      console.error(error);
     }
+    
+  };
 
-    render() { 
-        return (
-            <div className="dashboard-card">
-            <div className="container-row">
-                <div className="container-column">
-                    <h3>
-                        {this.props.Expir.name}</h3 >
-                     <h3>   {this.props.Expir.companyName}</h3>
-                       <h3> {this.props.Expir.startDate}</h3>
-                       <h3> {this.props.Expir.endDate}
-                    </h3>
-                </div>
-                <div className="container-column">
-                    <button onClick={this.toggelvisible} className="dashboard-btns edit">
-                        <img src={edit} alt="" ></img>
-                    </button>
-                    <button className="dashboard-btns cancel">
-                        <img src={trash} alt=""></img>
-                    </button>
-                </div>
-            </div>
-            {!this.state.isvisible && 
-            <div className="editinformation">
-                <div className="allinfo">
-                    <label htmlFor="major">Name</label>
-                    <input type="text" />
-                    <label htmlFor="degre">companyName</label>
-                    <input type="text" />
-                    <label htmlFor="degre">startDate</label>
-                    <input type="text" />
-                    <label htmlFor="degre">endDate</label>
-                    <input type="text" />
-                    <label htmlFor="degre">description</label>
-                    <input type="text" />
-                </div>
-                <div className="submit">
-                    <button>
-                        <img src={send} alt="" />
-                    </button>
-                </div>
-            </div>}
-        </div>
-        );
+  deleteData = async (id) => {
+    id = this.state.id
+    try {
+      const response = await axios.delete(
+        `http://localhost:5008/dashboard/experience/delete/${id}`
+      );
+      console.log(response.data.response);
+    } catch (error) {
+      console.log("error deleting dashboard", error);
+      console.error(error);
     }
+  };
+  handleChange = (event) => {
+    this.setState({
+      data: { ...this.state.data, [event.target.name]: event.target.value },
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.updateData(this.state.data, this.state.id);
+  };
+
+ formatDate=(date) =>{
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(date).toLocaleDateString('en-US', options);
+  };  
+  render() {
+    // alert(this.state.data.name)
+    const { Expir } = this.props;
+    console.log("Expir: ", Expir.name);
+
+    return (
+      <div className="dashboard-card">
+        <div className="container-row">
+          <div className="container-column">
+            <h3>{this.props.Expir.name}</h3>
+            <h3> {this.props.Expir.companyName}</h3>
+            <h3> {this.props.Expir.startDate}</h3>
+            <h3> {this.props.Expir.endDate}</h3>
+            <p>{this.props.Expir.description}</p>
+          </div>
+          <div className="container-column">
+            <button
+              onClick={this.toggelvisible}
+              className="dashboard-btns edit"
+            >
+              <img src={edit} alt=""></img>
+            </button>
+            <button className="dashboard-btns cancel">
+              <img src={trash} alt="DeleteMe" onClick={this.deleteData}></img>
+            </button>
+          </div>
+        </div>
+        {!this.state.isvisible && (
+          <div className="editinformation">
+            <div className="allinfo">
+              <form onSubmit={this.handleSubmit}>
+                <label htmlFor="major">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.data.name}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="degre">companyName</label>
+                {console.log(this.state.data.name)}
+                {console.log("jhhhihihihj"+this.props.Expir.CompanyName)}
+                <input
+                  type="text"
+                  name="companyName"
+                  value={this.state.data.companyName}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="degre">startDate</label>
+                <input
+                  type="date"
+                  name="startDate"
+                  value={this.state.data.startDate}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="degre">endDate</label>
+                <input
+                  type="date"
+                  name="endDate"
+                  value={this.state.data.endDate}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="degre">description</label>
+                <input
+                  type="text"
+                  name="description"
+                  value={this.state.data.description}
+                  onChange={this.handleChange}
+                />
+                <button type="submit" value="Update" className="submit">
+                  <img src={send} alt="" />
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
- 
+
 export default Expirence;

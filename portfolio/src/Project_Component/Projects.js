baa9d8e6efcd7743de1aae9e96bdafcdb1fdec9b
Mazen El Ali <mazenelali>
2023-02-10 20:00:50 +0200

    Mizo fix css

--- portfolio/src/Project_Component/Projects.js
+++ portfolio/src/Project_Component/Projects.js
@@ -43,4 +43,4 @@ export default Projects ;
                 //  "https://i.pinimg.com/564x/43/a9/c3/43a9c358ae61987d07e13847fe5e45d5.jpg"
                 // "https://i.pinimg.com/564x/5f/7c/08/5f7c08b0ac597d7c114c4f35fafc6d99.jpg"
                 // 'https://i.pinimg.com/564x/05/83/fd/0583fd2df28236b5baad40a5f7786108.jpg'
-                // "https://i.pinimg.com/564x/d9/88/01/d98801a6219bb43c5d7001309671f78c.jpg"
+                

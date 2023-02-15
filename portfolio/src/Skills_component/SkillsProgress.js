082a93ea6e1991615da8b95150f00ebcbe8b5383
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-02-15 19:53:07 +0200

    add animation

--- portfolio/src/Skills_component/SkillsProgress.js
+++ portfolio/src/Skills_component/SkillsProgress.js
@@ -9,34 +9,47 @@ class SkillsProgress extends React.Component {
         super(props);
     }
     state = {
-       
+       value:0
     }
-    render() {
-        return (
+    count  =  setInterval(() => {
+            
+                this.setState({value:this.state.value +1})
+                if (this.state.value === this.props.skil.percentage) {
+                  this.setState({ value: this.props.skil.percentage });
+                }
+          
+        }, 40);
         
-            <div className="circle">
-                <div className="Skills_Name">
-                    <h4> {this.props.skil.name}</h4>
-                </div>
-                <div className="skills_Percentage">
-                    <CircularProgressbar value={this.props.skil.percentage} text={`${this.props.skil.percentage}%`} className="CircularProgressbar"
-                        styles={buildStyles({
-                        rotation: 0,
-                        strokeLinecap: '',
-                        textSize: '16px',
-                        pathTransitionDuration: 0.5,
-                        pathColor: `#858792`,
-                        textColor: '#FFF',
-                        trailColor: '#d6d6d6',
-                        backgroundColor: '#3e98c7',
-                        })}
-                        
-                    />
-                </div>
-            </div>
+    render() {
 
+        
+        return (
+          <div className="circle">
+            <div className="Skills_Name">
+              <h4> {this.props.skil.name}</h4>
+            </div>
+            <div className="skills_Percentage">
+              <CircularProgressbar
+                value={this.state.value}
+                text={`${this.state.value}%`}
+                className="CircularProgressbar"
+                styles={buildStyles({
+                  rotation: 0,
+                  strokeLinecap: "",
+                  textSize: "16px",
+                  pathTransitionDuration: 0.5,
+                  pathColor: `#858792`,
+                  textColor: "#FFF",
+                  trailColor: "#d6d6d6",
+                  backgroundColor: "#3e98c7",
+                })}
+              />
+            </div>
+          </div>
         );
     }
 }
 
 export default SkillsProgress;
+
+//{this.props.skil.percentage}
